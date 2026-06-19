cfac6908ad4be563ad70ef6659a6a5dc14c11a8c
RookieMy <melih.yigit691@gmail.com>
2026-06-19 04:26:19 +0300

    core clicker mechanic implemented via button

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -44,4 +44,9 @@ public class GameManager : MonoBehaviour
         totalGold += amount;
         OnGoldChanged?.Invoke(totalGold);
     }
+
+    public void DamageTarget()
+    {
+        AddGold(1);
+    }
 }
